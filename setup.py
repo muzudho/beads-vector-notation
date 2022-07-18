9e3dc97f0a11f1b1d131526f75bcfe6abf765207
むずでょ <muzudho1@gmail.com>
2022-07-18 20:42:07 +0900

    A が不可

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="beadsnum",
-    version="1.0.0",
+    version="1.2.1",
     author="muzudho",
     author_email="muzudho1@gmail.com",
     description="Beads nested number notation package",
--- src/beadsnum/__init__.py
+++ src/beadsnum/__init__.py
@@ -4,8 +4,8 @@ import re
 class BeadsNum:
     """Beads Nested Number"""
 
-    # めんどくさいので .upper() して A,O と数字で構成されていればOkとする
-    pattern1 = re.compile(r"^([AO\d]*)$")
+    # めんどくさいので .upper() して O と数字で構成されていればOkとする
+    pattern1 = re.compile(r"^([O\d]*)$")
 
     def __init__(self, value=0):
 
@@ -53,39 +53,6 @@ class BeadsNum:
 
             # タプルとして格納する
             self._numbers = tuple(numerics)
-
-        """
-        try:
-            # 整数かどうか判定
-            num = int(str(value), 10)
-        except ValueError:
-            # 整数ではなかった
-            result = DicOrdNum.pattern.match(value)
-            if result:
-                # 構文は合っているようだ
-                prefix = result.group(1)
-                numeric = result.group(2)
-
-                # 桁数比較
-                if len(prefix) + 1 == len(numeric):
-                    # Aの個数が合っていた
-                    self._num = int(numeric)
-                else:
-                    # Aの個数が合っていない
-                    raise ValueError(f"not dictionary ordinal number: {value}")
-            else:
-                # 構文エラー
-                raise ValueError(f"not dictionary ordinal number: {value}")
-        else:
-            # 整数だ
-            if num < 0:
-                # 負数だ
-                raise ValueError(f"not dictionary ordinal number: {value}")
-            else:
-                # 正の数だ
-                self._num = value
-        """
-        pass
 
     def __str__(self):
         text = ""
