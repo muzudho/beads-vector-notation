157ffb94c1cc04b6fa8b819794ad975eeea5fa6c
むずでょ <muzudho1@gmail.com>
2022-07-18 19:16:50 +0900

    WIP

--- src/beadsnum/__init__.py
+++ src/beadsnum/__init__.py
@@ -4,8 +4,8 @@ import re
 class BeadsNum:
     """Beads Nested Number"""
 
-    # めんどくさいので A,O,o と数字で構成されていればOkとする
-    pattern1 = re.compile(r"^([AOo\d]*)$")
+    # めんどくさいので .upper() して A,O と数字で構成されていればOkとする
+    pattern1 = re.compile(r"^([AO\d]*)$")
 
     def __init__(self, value=0):
 
@@ -27,23 +27,32 @@ class BeadsNum:
         else:
             # それ以外は文字列として扱う
 
-            # A,O,o と数字で構成されている必要がある
+            # 大文字に変換
+            value = value.upper()
+
+            # A,O と数字で構成されている必要がある
             result = BeadsNum.pattern1.match(value)
             if result:
                 pass
             else:
                 raise ValueError(f"not beads nested number: {value}")
 
-            # O が２連続してはいけない（Ignore case）
-            if "OO" in value.upper():
+            # O が２連続してはいけない
+            if "OO" in value:
                 raise ValueError(f"not beads nested number: {value}")
 
-            # 先頭に O または o が付いているのは構わないものとし、
-            # 先頭に付いている O または o は削除する
-            value = value.lstrip('Oo')
+            # 先頭に O が付いているのは構わないものとし、
+            # 先頭に付いている O は削除する
+            value = value.lstrip('O')
+
+            # 区切り文字 O で分割
+            numerics = value.split('O')
+
+            # 整数化
+            numerics = map(lambda x: int(x), numerics)
 
             # タプルとして格納する
-            self._content = value,
+            self._content = tuple(numerics)
 
         """
         try:
