e1a51b94ffc346aa95f758a1ddff8a382c4fe2d2
むずでょ <muzudho1@gmail.com>
2022-07-21 22:56:13 +0900

    Reduce

--- src/beadsvec/__init__.py
+++ src/beadsvec/__init__.py
@@ -1,25 +1,17 @@
-import re
-from dicordnum import DicOrdNum
-
-
 class BeadsVec:
     """Beads Nested Number"""
-
-    # めんどくさいので .upper() して 'O' と数字で構成されていればOkとする
-    # 辞書順記数法に対応するために、めんどくさいので '_', 'A' が含まれていてもOkとする
-    pattern1 = re.compile(r"^([_AO\d]*)$")
 
     def __init__(self, value=0):
 
         # 整数かどうか判定
         try:
-            int_column = int(str(value), 10)
+            int_value = int(str(value), 10)
         except ValueError:
             # 整数でなければ次へ
             pass
         else:
             # タプルとして格納する
-            self._elements = int_column,
+            self._elements = int_value,
             return
 
         # タプル型なら
@@ -31,61 +23,34 @@ class BeadsVec:
         # それ以外は文字列として扱う
 
         # 大文字に変換
-        value = value.upper()
-
-        # '_', 'A', 'O' と数字で構成されている必要がある
-        result = BeadsVec.pattern1.match(value)
-        if result:
-            pass
-        else:
-            raise ValueError(f"not beads vector: {value}")
-
-        # 先頭に O が付いているのは構わないものとし、
-        # 先頭に付いている O は削除する
-        value = value.lstrip('O')
+        text = value.upper()
 
         new_element_list = []
 
-        # 区切り文字 O で分割
-        tokens = value.split('O')
+        # 区切り文字 'O' で分割
+        tokens = text.split('O')
 
         for token in tokens:
-            if token[:1] == 'A':
-                # A を除去する
-                n = int(token.replace('A', ''))
-                new_element_list.append(n)
+            # 整数かどうか判定
+            try:
+                int_element = int(str(token), 10)
+                new_element_list.append(int_element)
 
-            elif token[:1] == '_':
-                # まず '_' を除去する
-                token = token.replace('_', '')
-
-                figure = len(token)
-                modulo = 1
-                for i in range(0, figure):
-                    modulo *= 10
-
-                z = -1 * (modulo - int(token))
-                new_element_list.append(z)
-
-            else:
-                n = int(token)
-                new_element_list.append(n)
+            except ValueError:
+                # 整数でなければ
+                raise ValueError(f"no beads vector notation: {value}")
 
         # タプルとして格納する
         self._elements = tuple(new_element_list)
 
     def __str__(self):
-        """辞書順記数法"""
+        """数珠玉記数法"""
         text = ""
         for token in self._elements:
-            # print(
-            #    f"token:{token} DicOrdNum(token):{DicOrdNum(token)} text:{text}")
-            text = f"{text}o{DicOrdNum(token)}"
+            text = f"{text}o{token}"
 
-        # 先頭だけを大文字の 'O' にする
-        # print(f"text:{text}")
-        text = f"O{text[1:]}"
-        return text
+        # 先頭の 'o' は除去します
+        return text.lstrip('o')
 
     @property
     def elements(self):
--- tests/test.py
+++ tests/test.py
@@ -5,60 +5,46 @@ from src.beadsvec import BeadsVec
 
 test_data = [
     # 正の零
-    ['0', BeadsVec(0), 'O0', (0,)],
-    # -   -----------   --   ----
-    # 1   2             3    3
+    ['0', BeadsVec(0), '0', (0,)],
+    # -   -----------   -   ----
+    # 1   2             3   3
     # 1. Test case text
     # 2. Actual
     # 3. Expected
 
+    # 負の零（正の零になります）
+    ['-0', BeadsVec(-0), '0', (0,)],
+
     # 正の整数
-    ['1', BeadsVec(1), 'O1', (1,)],
-    ['10', BeadsVec(10), 'OA10', (10,)],
+    ['1', BeadsVec(1), '1', (1,)],
+    ['10', BeadsVec(10), '10', (10,)],
 
-    # 正の整数の文字列
-    ['"0"', BeadsVec('0'), 'O0', (0,)],
-    ['"1"', BeadsVec('1'), 'O1', (1,)],
-    ['"10"', BeadsVec('10'), 'OA10', (10,)],
+    # 負の整数
+    ['-1', BeadsVec(-1), '-1', (-1,)],
 
-    # 1o2 の形
-    #['"1o2"', BeadsVec('1o2')],
-    #['"1o2o3"', BeadsVec('1o2o3')],
-    #['"1o2o3o4"', BeadsVec('1o2o3o4')],
+    # 整数の文字列
+    ['"-1"', BeadsVec('-1'), '-1', (-1,)],
+    ['"0"', BeadsVec('0'), '0', (0,)],
+    ['"1"', BeadsVec('1'), '1', (1,)],
+    ['"10"', BeadsVec('10'), '10', (10,)],
 
-    # O1o2 の形
-    ['"O1o2o_9"', BeadsVec('O1o2o_9'), 'O1o2o_9', (1, 2, -1)],
-    ['"O1o2"', BeadsVec('O1o2'), 'O1o2', (1, 2)],
-    ['"O1o2oA10o4"', BeadsVec('O1o2oA10o4'), 'O1o2oA10o4', (1, 2, 10, 4)],
+    # 数珠玉記数法 デフォルト（snake_case）
+    ['"1o2o-1"', BeadsVec('1o2o-1'), '1o2o-1', (1, 2, -1)],
+    ['"1o2"', BeadsVec('1o2'), '1o2', (1, 2)],
+    ['"1o2o10o4"', BeadsVec('1o2o10o4'), '1o2o10o4', (1, 2, 10, 4)],
 
-    # o1o2 の形, snake_case への寛容
-    ['"o1o2o_9"', BeadsVec('o1o2o_9'), 'O1o2o_9', (1, 2, -1)],
-    ['"o1o2"', BeadsVec('o1o2'), 'O1o2', (1, 2)],
-    ['"o1o2oA10o4"', BeadsVec('o1o2oA10o4'), 'O1o2oA10o4', (1, 2, 10, 4)],
-
-    # O1O2 の形
-    ['"O1O2O_9"', BeadsVec('O1O2O_9'), 'O1o2o_9', (1, 2, -1)],
-    ['"O1O2"', BeadsVec('O1O2'), 'O1o2', (1, 2)],
-    ['"O1O2OA10O4"', BeadsVec('O1O2OA10O4'), 'O1o2oA10o4', (1, 2, 10, 4)],
+    # 数珠玉記数法 UPPER_SNAKE_CASE
+    ['"1O2O-1"', BeadsVec('1O2O-1'), '1o2o-1', (1, 2, -1)],
+    ['"1O2"', BeadsVec('1O2'), '1o2', (1, 2)],
+    ['"1O2O10O4"', BeadsVec('1O2O10O4'), '1o2o10o4', (1, 2, 10, 4)],
 
     # 混在
-    ['"o_9O1oA10"', BeadsVec('o_9O1oA10'), 'O_9o1oA10', (-1, 1, 10)],
-
-    # 拡張: 辞書順記数法への寛容
-    ['"A77"', BeadsVec('A77'), 'OA77', (77,)],
-    ['"AA777"', BeadsVec('AA777'), 'OAA777', (777,)],
-    ['"O7oA77oAA777oAAA7777"', BeadsVec(
-        'O7oA77oAA777oAAA7777'), 'O7oA77oAA777oAAA7777', (7, 77, 777, 7777)],
-
-    ['"a77"', BeadsVec('a77'), 'OA77', (77,)],
-    ['"aa777"', BeadsVec('aa777'), 'OAA777', (777,)],
-    ['"o7oa77oaa777oaaa7777"', BeadsVec(
-        'O7oa77oaa777oaaa7777'), 'O7oA77oAA777oAAA7777', (7, 77, 777, 7777)],
+    ['"-1O1o10"', BeadsVec('-1O1o10'), '-1o1o10', (-1, 1, 10)],
 
     # タプル
-    ['(1,2)', BeadsVec((1, 2)), 'O1o2', (1, 2)],
-    ['(1,2,-3)', BeadsVec((1, 2, -3)), 'O1o2o_7', (1, 2, -3)],
-    ['(1,2,10,4)', BeadsVec((1, 2, 10, 4)), 'O1o2oA10o4', (1, 2, 10, 4)],
+    ['(1,2)', BeadsVec((1, 2)), '1o2', (1, 2)],
+    ['(1,2,-3)', BeadsVec((1, 2, -3)), '1o2o-3', (1, 2, -3)],
+    ['(1,2,10,4)', BeadsVec((1, 2, 10, 4)), '1o2o10o4', (1, 2, 10, 4)],
 ]
 
 for datum in test_data:
@@ -72,12 +58,12 @@ for datum in test_data:
 
 # B は使えない文字だ
 try:
-    print(f'[Error] "OB1" --> {BeadsVec("OB1")}')
+    print(f'[Error] "B1" --> {BeadsVec("B1")}')
 except:
-    print(f'"OB1" is not BeadsVec')
+    print(f'"B1" is not BeadsVec')
 
 # o が２連続するのはおかしい
 try:
-    print(f'[Error] "O2oo1" --> {BeadsVec("O2oo1")}')
+    print(f'[Error] "2oo1" --> {BeadsVec("2oo1")}')
 except:
-    print(f'"O2oo1" is not BeadsVec')
+    print(f'"2oo1" is not BeadsVec')
